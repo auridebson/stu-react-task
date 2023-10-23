7f6618ed30d6f77a60e289e3f2ccc8a6f5e73aa3
Auridebson Sousa <auridebson@vutus.com>
2023-10-23 08:33:56 -0300

    Update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 #Router-dom
 npm i --save react-router-dom firebase react-toastify
+
+#Toastify
+$ npm install --save react-toastify
--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Home from "./pages/Home";
 import About from './pages/About';
 import AddEdit from './pages/AddEdit';
 import View from './pages/View';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
  
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <AddEdit />
-      <View />
-    </div>
+
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position='top-center' />
+        <Switch>
+          <Router exact path="/" component={Home} />
+          <Router path="/add" component={AddEdit} />
+          <Router path="/update/:id" component={AddEdit} />
+          <Router path="/view/:id" component={View} />
+          <Router path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+    
   );
 }
