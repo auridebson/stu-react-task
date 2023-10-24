e43a8db27e0c56787fc40f5c311d440bfce7688b
Auridebson Lima <auridebson@vutus.com>
2023-10-24 04:48:37 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/auridebson/stu-react-task""
    This reverts commit 5dd94ac79c073d50459a5ec119677ac39f5763de.

---
+++ src/pages/View/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function View() {
+    return (
+        <div>
+            <h2>
+                Página View - App Tasks
+            </h2>
+        </div>
+    )
+}
