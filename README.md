e7d660e5f4c180e745d4f2f29dcf87d885b1576b
Auridebson Lima <auridebson@vutus.com>
2023-10-24 05:01:21 -0300

    Update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Video: https://www.youtube.com/watch?v=zEdI9L1MZU8&t=877s
-
 -- Instalar dependências
 
 #Router-dom
 npm i --save react-router-dom firebase react-toastify
 
-
+#Toastify
+-- https://www.npmjs.com/package/react-toastify
+$ npm install --save react-toastify
--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import AddPage from "./pages/AddPage";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
+import About from './pages/About';
+import AddEdit from './pages/AddEdit';
+import View from './pages/View';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
-
+ 
 export default function App() {
   return (
-    <div className="App">
-      <h1>
-        Minha página de tarefas
-      </h1>
 
-      <Home />
-
-      <AddPage />
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <ToastContainer position='top-center' />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={AddEdit} />
+          <Route path="/update/:id" component={AddEdit} />
+          <Route path="/view/:id" component={View} />
+          <Route path="/about" component={About} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
